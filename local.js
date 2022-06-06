8c5615f15cd7253c8d360db0d2440e277cdfd406
The A005 Conglomerate <106974866+tacltd@users.noreply.github.com>
2022-06-06 15:26:20 +0000

    Update local.js

--- local.js
+++ local.js
@@ -1,1 +1,26 @@
+// TOGGLE MENU
+function toggleMenu() {
+  var home = $('.home.section-container '),
+      about = $('.about.section-container '),
+      yc = $('.yc.section-container '),
+      invest = $('.invest.section-container '),
+      contact = $('.contact.section-container a.contact');
+  $('.contact.section-container a.contact').on('click', function() {
+    $('.contact.section-container .menu').toggleClass('show');
+  })
+  
+  
+  $('div.content').on('click', function() {
+    if ($('.contact.section-container .menu').hasClass('show')) {
+      $('.contact.section-container .menu').removeClass('show');
+    }
+  })
+}
 
+
+
+
+// WINDOW ON LOAD
+window.onload = function() {
+  toggleMenu();
+}
